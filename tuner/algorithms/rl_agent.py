d28169045af8056378617573167bc2689d33e478
Acdimy <49313774+Acdimy@users.noreply.github.com>
2021-07-23 15:27:46 +0800

    test

--- tuner/algorithms/rl_agent.py
+++ tuner/algorithms/rl_agent.py
@@ -97,6 +97,7 @@ class RLAgent:
 
     def fit(self, steps, nb_max_episode_steps=50, verbose=2):
         print("Come into rl fit...")
+        print(self.env.db.knobs.names())
         self.agent.fit(self.env, nb_steps=steps, nb_max_episode_steps=nb_max_episode_steps, verbose=verbose, visualize=True)
         print("Come outside rl fit...")
 
