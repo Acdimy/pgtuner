cef1ac758a329e6558434bdee213ec557e9c1f5a
Acdimy <49313774+Acdimy@users.noreply.github.com>
2021-07-26 16:27:23 +0800

    bad case

--- tuner/db_agent.py
+++ tuner/db_agent.py
@@ -301,7 +301,7 @@ class DB_Agent:
             if name == "shared_buffers":
                 print("setting shared_buffers")
                 # self.exec_command_on_host("pg_ctl restart -D {data_path}".format(data_path=self.data_path), ignore_status_code=True)
-                self.exec_command_on_host("pg_ctl stop -D {data_path}".format(data_path=self.data_path), ignore_status_code=True, timeout=60)
+                # self.exec_command_on_host("pg_ctl stop -D {data_path}".format(data_path=self.data_path), ignore_status_code=True, timeout=60)
                 # print("Stopped")
                 self.exec_command_on_host("pg_ctl start -D {data_path}".format(data_path=self.data_path), ignore_status_code=True, timeout=60)
                 print("end setting shared_buffers")
