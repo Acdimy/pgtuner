14c259a9503d9eab8416e7d179f6069644f090c8
Acdimy <49313774+Acdimy@users.noreply.github.com>
2021-07-26 15:28:42 +0800

    end setting shared_buffers

--- tuner/db_agent.py
+++ tuner/db_agent.py
@@ -301,6 +301,7 @@ class DB_Agent:
             if name == "shared_buffers":
                 print("setting shared_buffers")
                 self.exec_command_on_host("pg_ctl restart -D {data_path}".format(data_path=self.data_path), ignore_status_code=True)
+                print("end setting shared_buffers")
             # self.exec_command_on_host("gs_guc reload -c \"%s=%s\" -D %s" % (name, value, self.data_path))
         except ExecutionError as e:
             if str(e).find('Success to perform gs_guc!') < 0:
